8e0ff0063d6ed7d2a4ef350e340fcfcc21e65180
MehrotraAyush <ayushmehrotra06092001@gmail.com>
2026-05-29 19:04:34 +0530

    SortingQuestion

--- src/Searching/Sqrt.java
+++ src/Searching/Sqrt.java
@@ -1,8 +1,8 @@
 package Searching;
 
 public class Sqrt {
-    public static int findSqrt(int n){
-        int ans=-1;
+    public static double findSqrt(int n){
+        double ans=-1;
         int s=1;
         int e=n;
 
@@ -24,6 +24,28 @@ public class Sqrt {
                 s=mid+1;
             }
         }
+
+        double factor=1;
+        int totalPresison=3;
+
+        for(int round=1;round<=totalPresison;round++){
+            factor=factor/10;
+            for(int i=1;i<=10;i++){
+
+                double newAns=ans+factor;
+
+                if(newAns*newAns==n){
+                    return newAns;
+                }
+                else if (newAns*newAns<=n) {
+                    ans=newAns;
+                }
+                else
+                    break;
+            }
+
+        }
+        ans=Math.round(ans*1000.0)/1000.0;
         return ans;
     }
 
